6b3a6e2b1fd9ddfb1fea6f7284a99e0437b4622b
Wolfleader <james.morgan@iinet.net.au>
2021-06-28 12:45:13 +0800

    added ability to add extra components based on name

--- Assets/Scripts/PawnSpawner/PawnSpawner.cs
+++ Assets/Scripts/PawnSpawner/PawnSpawner.cs
@@ -5,35 +5,39 @@ using UnityEngine;
 
 public class PawnSpawner : MonoBehaviour
 {
-    private void Awake()
-    {
-        throw new NotImplementedException();
-    }
+    [SerializeField] private GameObject playerPrefab;
+    [SerializeField] private List<string> components;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        var player = Instantiate(playerPrefab, transform.position, transform.rotation);
+
+        foreach (var component in components)
+        {
+            Type type = System.Type.GetType(component);
+
+            player.AddComponent(type);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
-    
+
     private void OnDrawGizmos()
     {
         var pos = transform.position;
 
         var radius = 0.75f;
-        
+
         Gizmos.color = Color.cyan;
         Gizmos.DrawWireSphere(pos, radius);
-        
+
         Gizmos.color = Color.red;
         pos.z += radius;
         Gizmos.DrawLine(pos, pos + transform.forward * 2);
     }
-
 }
