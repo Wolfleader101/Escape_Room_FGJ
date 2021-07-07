b9a165a8fb5fc975c207f6c2dbe8a77f6ab173d8
triKUMA <trikuma@hotmail.com>
2021-07-07 22:39:19 +0800

    Added comments.

--- Assets/Scripts/Interactables/Lasers/LaserReceiver.cs
+++ Assets/Scripts/Interactables/Lasers/LaserReceiver.cs
@@ -16,9 +16,11 @@ public class LaserReceiver : InteractableBase {
 
     [HideInInspector] public int cutoff;
 
+    // Counter used to make sure the receiver can be read as active by other objects for a whole frame before deactivating.
     private int stepSinceInactive = 0;
 
     private void LateUpdate() {
+        // Increment counter, will be reset to 0 next frame if still active. If the counter gets to 2, the receiver has been active for a full frame and can now be deactivated.
         stepSinceInactive++;
         if(stepSinceInactive >= 2) {
             Deactivate();
