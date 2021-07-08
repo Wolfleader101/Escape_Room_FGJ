06bdb4ce1d5f08a77614e3cd4fbe117da0e96cd1
Wolfleader <james.morgan@iinet.net.au>
2021-07-08 18:57:29 +0800

    messing around

---
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    [SerializeField] private List<Item> items;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
